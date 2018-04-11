c10356b66dd6e993c095512d9a5e8ac18b4c8b30
ChenZexin-PC\ChenZexin <zexin@and2girls.com>
2018-04-11 23:58:10 +0800

    4-11修改

--- opengl-console/drawObject.h
+++ opengl-console/drawObject.h
@@ -7,23 +7,28 @@
 class drawobject
 {
 public:
-	explicit drawobject(const GLfloat *v = nullptr, const GLuint *e = nullptr);
+	explicit drawobject();
+	void init_data(const GLfloat *v, const GLuint num_v, const GLuint *e, const GLuint num_e);
 	~drawobject();
 	GLboolean load();
 	void unload();
 	GLboolean draw(bool auto_load);
 	const GLfloat* vertexs;
+	GLuint num_vertex;
 	const GLuint* elements;
-	const vector3 position = vector3::zero;
-	const quaternion rotation = quaternion::zero;
+	GLuint num_element;
+	vector3 position = vector3::zero;
+	quaternion rotation = quaternion::zero;
 	GLuint vao[1];
-	enum buff_id
-	{
-		vbo,
-		ebo,
-		num,
-	};
-	GLuint buff[buff_id::num];
+	GLuint vbo[1];
+	GLuint ebo[1];
+	//enum buff_id
+	//{
+	//	vbo,
+	//	ebo,
+	//	num,
+	//};
+	//GLuint buff[buff_id::num];
 	GLuint count_element;
 	GLboolean loaded;
 };
--- opengl-console/drawobject.cpp
+++ opengl-console/drawobject.cpp
@@ -1,38 +1,57 @@
 #include "DrawObject.h"
 #include "GL3/gl3w.h"
 
-drawobject::drawobject(const GLfloat* v, const GLuint* e) : vertexs(v), elements(e)
+drawobject::drawobject()
 {
+}
+
+void drawobject::init_data(const GLfloat *v, const GLuint num_v, const GLuint *e, const GLuint num_e)
+{
+	vertexs = v;
+	num_vertex = num_v;
+	elements = e;
+	num_element = num_e;
 }
 
 drawobject::~drawobject()
 {
+	//unload();
+	//delete[] vao;
+	//delete[] buff;
+	//delete vertexs;
+	//delete elements;
+	//delete &position;
+	//delete &rotation;
 }
 
 GLboolean drawobject::load()
 {
-	if (vertexs == nullptr || elements == nullptr || vao != 0)
+	if (vertexs == nullptr || elements == nullptr)
 	{
 		return GL_FALSE;
 	}
 	glGenVertexArrays(1, vao);
-	glGenBuffers(buff_id::num, buff);
 	glBindVertexArray(vao[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, buff[buff_id::vbo]);
-	glBufferStorage(GL_ARRAY_BUFFER, sizeof(vertexs), vertexs, 0);
+	glCreateBuffers(1, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferStorage(GL_ARRAY_BUFFER, num_vertex, vertexs, 0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, buff[buff_id::ebo]);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buff[buff_id::ebo]);
-	glBufferStorage(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, 0);
+	
+	glGenBuffers(1, ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
+	glBufferStorage(GL_ELEMENT_ARRAY_BUFFER, num_element, elements, 0);
+	count_element = num_element / sizeof(GLuint);
+
 	loaded = true;
-	return true;
+	return GL_TRUE;
 }
 
 void drawobject::unload()
 {
 	glDeleteBuffers(1, vao);
-	glDeleteBuffers(buff_id::num, buff);
+	//glDeleteBuffers(buff_id::num, buff);
+
 	loaded = GL_FALSE;
 }
 
@@ -50,7 +69,7 @@ GLboolean drawobject::draw(const bool auto_load)
 		}
 	}
 	glBindVertexArray(vao[0]);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buff[buff_id::ebo]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
 	glDrawElements(GL_TRIANGLES, count_element, GL_UNSIGNED_INT, nullptr);
 	return GL_TRUE;
 }
